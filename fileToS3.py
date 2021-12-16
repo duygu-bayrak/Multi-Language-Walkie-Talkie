6bdaa7764561fed67f0c99f93f1e9fde8cb32d04
Duygu Bayrak <duygu98@web.de>
2021-12-16 18:38:15 +0800

    send json with audio base64 encoding to s3 bucket

--- fileToS3.py
+++ fileToS3.py
@@ -1,4 +1,8 @@
+#send {user id, original language, target language, original message (audio file)} via JSON to S3
+
 import boto3
+import base64
+import json
 
 # specify credentials
 aws_access_key_id=""
@@ -14,10 +18,20 @@ aws_session_token=aws_session_token
 
 bucket_name = "walkietalkie.testupload" #name of your s3 bucket
 
+audio = open('C:/Users/user/Desktop/transcribe-sample.mp3', 'rb')
+payload = {}
+payload['user_id'] = 1
+payload['source_lang'] = "en"
+payload['target_lang'] = "de"
+payload['audio'] = base64.b64encode(audio.read()).decode('utf-8') #need to decode cause "binary is not JSON serializable"
+js = json.dumps(payload)
+
+#fileData = base64.b64decode(payload['audio']) #to read back
+
 #Creating S3 Resource From the Session.
 s3 = session.resource('s3')
-object = s3.Object(bucket_name, "audio.mp3") #specify how file should be named
-result = object.put(Body=open('C:/Users/user/Desktop/transcribe-sample.mp3', 'rb')) #path to local file
+object = s3.Object(bucket_name, "audio.json") #specify how file should be named
+result = object.put(Body=js) #ContentType = 'application/json'
 
 res = result.get('ResponseMetadata')
 if res.get('HTTPStatusCode') == 200:
