0342ee9d7128db868a84a75b78a9ffe90945fd1d
eccdbbabcdeee <eccdbbabcdeee@gmail.com>
2021-12-28 14:49:25 +0800

    set elastic IP

--- client.py
+++ client.py
@@ -82,7 +82,7 @@ class GUI:
         
     def initialize_socket(self):
         self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # initialazing socket with TCP and IPv4
-        remote_ip = 'ec2-54-208-62-46.compute-1.amazonaws.com' # 127.0.0.1'  # IP address
+        remote_ip = 'ec2-34-195-127-232.compute-1.amazonaws.com' # 127.0.0.1'  # IP address
         # remote_ip = '127.0.0.1'
         remote_port = 10319  # TCP port
         try:
