3945b6e6e2e36d0db5a86de2f926197f617ca47b
duygu-bayrak <57842901+duygu-bayrak@users.noreply.github.com>
2021-12-29 17:18:44 +0800

    Update fileToS3.py
    add room metadata

--- fileToS3.py
+++ fileToS3.py
@@ -16,26 +16,18 @@ aws_secret_access_key=aws_secret_access_key,
 aws_session_token=aws_session_token
 )
 
-bucket_name = "walkietalkie.testupload" #name of your s3 bucket
+bucket_name = "audio.walkietalkie" #name of your s3 bucket
 
 audio = open('C:/Users/user/Desktop/transcribe-sample.mp3', 'rb')
-#file = 'C:/Users/user/Desktop/transcribe-sample.mp3'
-payload = {}
-#payload['user_id'] = 1
-#payload['source_lang'] = "1" #store language code instead of string "en"
-#payload['target_lang'] = "2"
-#payload['audio'] = base64.b64encode(audio.read()).decode('utf-8') #need to decode cause "binary is not JSON serializable"
-#js = json.dumps(payload)
-
-#fileData = base64.b64decode(payload['audio']) #to read back
 
 #Creating S3 Resource From the Session.
 s3 = session.resource('s3')
 object = s3.Object(bucket_name, "audio.mp3") #specify how file should be named
 result = object.put(Body=audio.read(), Metadata={
       'user_id': "1",
-      'source_lang': "en",
-      'target_lang': "de"
+      'source_lang': "1",
+      'target_lang': "2",
+      'room': "456"
     }) 
 
 res = result.get('ResponseMetadata')
@@ -43,4 +35,14 @@ if res.get('HTTPStatusCode') == 200:
   print('File Uploaded Successfully')
 else:
   print('File Not Uploaded')
+
+#file = 'C:/Users/user/Desktop/transcribe-sample.mp3'
+#payload = {}
+#payload['user_id'] = 1
+#payload['source_lang'] = "1" 
+#payload['target_lang'] = "2"
+#payload['audio'] = base64.b64encode(audio.read()).decode('utf-8') #need to decode cause "binary is not JSON serializable"
+#js = json.dumps(payload)
+
+#fileData = base64.b64decode(payload['audio']) #to read back
 
