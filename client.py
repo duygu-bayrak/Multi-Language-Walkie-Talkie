2e65add32c101c3badfc1b5e8e8bb3b99fcf074d
eccdbbabcdeee <eccdbbabcdeee@gmail.com>
2021-12-29 20:10:41 +0800

    add TODO: play back audio from not self

--- client.py
+++ client.py
@@ -531,3 +531,5 @@ if __name__ == '__main__':
 # TODO test basic scenario:
 # user 1 @ room 1: en -> de
 # user 2 @ room 1: de -> en
+
+# TODO: on !DB_UPDATED, only play back audio if from another client (not from self)
