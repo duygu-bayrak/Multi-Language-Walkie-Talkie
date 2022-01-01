f9ab788bb3a8364785799d277392e47d32950c6d
eccdbbabcdeee <eccdbbabcdeee@gmail.com>
2022-01-01 19:35:03 +0800

    Create start_client.sh
    In general:
    
    python3 ./Multi-Language-Walkie-Talkie/client.py <user ID> <room> <port> <aws_access_key_id> <aws_secret_access_key> <aws_session_token> --original_language_ID <language ID> --target_language_ID <language ID> --OS <windows/linux>

---
+++ start_client.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 ./Multi-Language-Walkie-Talkie/client.py 2 10319 10319 <aws_access_key_id> <aws_secret_access_key> <aws_session_token> --original_language_ID 1 --target_language_ID 2 --OS linux
